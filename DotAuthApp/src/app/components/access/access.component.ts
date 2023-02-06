9fcdbac4746eee32770ac21b759aa4894253a2f4
mothupir <mothupir@bbd.co.za>
2023-02-06 20:13:43 +0200

    updated user access

--- DotAuthApp/src/app/components/access/access.component.ts
+++ DotAuthApp/src/app/components/access/access.component.ts
@@ -9,15 +9,16 @@ import { SpinnerService } from 'src/app/services/spinner/spinner.service';
   styleUrls: ['./access.component.css']
 })
 export class AccessComponent {
+  organizationRegistered: boolean = false;
+  organization: string = "";
+
   accessLevels: string[] = [];
   accessLevel: string = "";
 
-  users: string[] = [];
-  user: string = "";
+  users: {[key: string]: string}[] = [];
+  userName: string = "";
+  userAddress: string = "";
   showUsersDialog: boolean = false;
-
-  organizationRegistered: boolean = false;
-  organization: string = "";
 
   constructor(
     private contractService: ContractService,
@@ -29,9 +30,10 @@ export class AccessComponent {
     this.checkOrganization();
   }
 
+  // ------------ Organization Management ------------
   async checkOrganization() {
     this.spinner.show();
-    await this.contractService.isOrganizationRegistered().then(async (data: boolean) => {
+    await this.contractService.isOrganizationRegistered().then(async (data: any) => {
       this.organizationRegistered = data;
       await this.getAccessLevels();
     }).catch((e: any) => {
@@ -45,33 +47,35 @@ export class AccessComponent {
     this.spinner.show();
     await this.contractService.createOrganization(this.organization).then(async () => {
       await this.checkOrganization();
+      this.organization = "";
     }).catch((e: any) => {
+      this.messageService.add({ severity: 'error', summary: 'Create Organization', detail: `Error. Could not create organization. ${e}` });
       console.log(e);
     }).finally(() => {
       this.spinner.hide();
     })
   }
 
+  // ------------ Access Level Management ------------
   async getAccessLevels() {
     this.spinner.show();
-    await this.contractService.getAccessLevels().then((data: string[]) => {
+    await this.contractService.getAccessLevels().then((data: any) => {
       this.accessLevels = data;
     }).catch((e: any) => {
-      console.error(e);
+      this.messageService.add({ severity: 'error', summary: 'Load Access Levels', detail: `Error. Could not load Access Levels. ${e}` });
     }).finally(() => {
       this.spinner.hide();
     });
   }
 
-  async createAccessLevel() {
+  async addAccessLevel() {
     this.spinner.show();
-    await this.contractService.createAccessLevel(this.accessLevel).then(async () => {
+    await this.contractService.addAccessLevel(this.accessLevel).then(async () => {
       await this.getAccessLevels();
       this.messageService.add({ severity: 'success', summary: 'Add Access Level', detail: `Access Level, ${this.accessLevel} added successfully...` });
       this.accessLevel = "";
     }).catch((e: any) => {
-      this.messageService.add({ severity: 'error', summary: 'Add Access Level', detail: `Access Level, ${this.accessLevel} could not be added.` });
-      console.error(e);
+      this.messageService.add({ severity: 'error', summary: 'Add Access Level', detail: `Access Level, ${this.accessLevel} could not be added. ${e}` });
     }).finally(() => {
       this.spinner.hide();
     });
@@ -80,29 +84,73 @@ export class AccessComponent {
   removeAccessLevel(accessLevel: string) {
     this.confirmationService.confirm({
       header: "Remove Access Level",
-      message: `Are you sure that you want to delete AccessLevel, ${accessLevel}?`,
+      message: `Are you sure that you want to remove Access Level, ${accessLevel}?`,
       accept: async () => {
-        await this.contractService.removeAccessLevel(accessLevel).then(() => {
+        this.spinner.show();
+        await this.contractService.removeAccessLevel(accessLevel).then(async () => {
+          await this.getAccessLevels();
           this.messageService.add({ severity: 'success', summary: 'Remove Access Level', detail: `Access Level, ${accessLevel} removed successfully...` });
         }).catch((e: any) => {
           this.messageService.add({ severity: 'error', summary: 'Remove Access Level', detail: `Access Level, ${accessLevel} could not be removed.` });
-          console.log(e);
-        }).finally(() => {
-          this.getAccessLevels();
+        }).finally(async () => {
+          this.spinner.hide();
         });
       }
     });
   }
 
-  async getUsers(accessLevel: string) {
-    this.showUsersDialog = true;
+  // ------------ User Management ------------
+  async getUsers(level: string) {
+    this.spinner.show();
+    await this.contractService.getUsers(level).then((data: any) => {
+      this.users = data;
+      this.accessLevel = level;
+    }).catch((e: any) => {
+      this.messageService.add({ severity: 'error', summary: 'Get Users', detail: "Error. Could not load users." });
+    }).finally(() => {
+      this.toggleUserDialog();
+      this.spinner.hide();
+    });
   }
 
-  async addUser(address: string) {
-
+  async addUser() {
+    this.spinner.show();
+    await this.contractService.addUser(this.userAddress, this.userName).then(async () => {
+      await this.getUsers(this.accessLevel);
+      this.messageService.add({ severity: 'success', summary: 'Add User', detail: `User, ${this.userName} added successfully...` });
+      }).catch((e: any) => {
+        this.messageService.add({ severity: 'error', summary: 'Add User', detail: `User, ${this.userName} could not be added. ${e}` });
+    }).finally(() => {
+      this.userAddress = "";
+      this.userName = "";
+      this.spinner.hide();
+    });
   }
 
-  async removeUser(address: string) {
-    
+  async removeUser(address: string, name: string) {
+    this.confirmationService.confirm({
+      header: "Remove User",
+      message: `Are you sure that you want to remove User, ${name}?`,
+      accept: async () => {
+        this.spinner.show();
+        await this.contractService.removeUser(address).then(async () => {
+          await this.getUsers(this.accessLevel);
+          this.messageService.add({ severity: 'success', summary: 'Remove User', detail: `User, ${name} removed successfully...` });
+        }).catch((e: any) => {
+          this.messageService.add({ severity: 'error', summary: 'Remove User', detail: `User, ${name} could not be removed. ${e}` });
+        }).finally(async () => {
+          this.spinner.hide();
+        });
+      }
+    });
+  }
+
+  toggleUserDialog() {
+    if(this.showUsersDialog) {
+      this.showUsersDialog = false;
+      this.accessLevel = "";
+    } else {
+      this.showUsersDialog = true;
+    }
   }
 }
--- DotAuthApp/src/app/services/contract/contract.service.ts
+++ DotAuthApp/src/app/services/contract/contract.service.ts
@@ -12,11 +12,11 @@ export class ContractService {
     this.provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   }
 
-  async isOrganizationRegistered(): Promise<any> {
+  async isOrganizationRegistered(): Promise<boolean> {
     return true;
   }
 
-  async createOrganization(name: string): Promise<any> {
+  async createOrganization(name: string): Promise<void> {
 
   }
 
@@ -24,11 +24,27 @@ export class ContractService {
     return ["Primary", "Secondary", "Tertiary"];
   }
 
-  async createAccessLevel(name: string): Promise<any> {
+  async addAccessLevel(name: string): Promise<void> {
 
   }
 
-  async removeAccessLevel(name: string): Promise<any> {
+  async removeAccessLevel(name: string): Promise<void> {
+
+  }
+
+  async getUsers(accessLevel: string): Promise<{[key: string]: string}[]> {
+    return [
+      { name: "User 1", address: "address1" },
+      { name: "User 2", address: "address2" },
+      { name: "User 3", address: "address3" }
+    ];
+  }
+
+  async addUser(address: string, name: string): Promise<void> {
+
+  }
+
+  async removeUser(address: string): Promise<void> {
 
   }
 
